b23e15014d399fb6ff8b0843359205cbfff8d8df
Max <freefpdie@gmail.com>
2018-07-29 19:51:30 +0700

    fix query

--- src/main/java/io/shifu/jobsearch/repository/JobRepository.java
+++ src/main/java/io/shifu/jobsearch/repository/JobRepository.java
@@ -11,14 +11,8 @@ import java.util.Optional;
 public interface JobRepository extends JpaRepository<Job, Long> {
 
     // можно разделить на несколько разных запросов
-    @Query(nativeQuery = true, value = "select * from job j where (?1 is null or j.salary >= ?1) " +
-            "and (?2 is null or j.location like %?2%) " +
-            "and (?3 is null or j.title like %?3%) " +
-            "and (?4 is null or j.description like %?4%)",
-            countQuery = "select count(*) from job j where (?1 is null or j.salary >= ?1) " +
-                    "and (?2 is null or j.location = %?2%) " +
-                    "and (?3 is null or j.title like %?3%) " +
-                    "and (?4 is null or j.description like %?4%)")
+    @Query(nativeQuery = true, value = "select * from job j where (?1 is null or j.salary >= ?1) and (?2 is null or j.location like %?2%) and (?3 is null or j.title like %?3%) and (?4 is null or j.description like %?4%)",
+            countQuery = "select count(*) from job j where (?1 is null or j.salary >= ?1) and (?2 is null or j.location = %?2%) and (?3 is null or j.title like %?3%) and (?4 is null or j.description like %?4%)")
     Page<Job> findBySearchQuery(Integer salary, String location, String title, String description, Pageable pageable);
 
     Optional<Job> findBySiteAndSiteId(String site, String siteId);
